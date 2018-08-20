7586199c9a745a1da6f4da8f1c4c7fbc80a71b9d
Kris3579 <kristianesvelt@hotmail.com>
2018-08-19 22:32:12 -0700

    All things complete, 1 known bug

---
+++ form-building/README.md
@@ -0,0 +1,25 @@
+# Project Name
+
+**Author**: Kris Esvelt
+**Version**: 1.0.0 (increment the patch/fix version number up if you make more commits past your first submission)
+
+## Overview
+Create a form for user's to input their own blog post for our page, and have a preview template of their blog appear as they fill in the form, otherwise known as "live-update".
+
+## Getting Started
+1. Create a form for the page.
+2. Add a event handler to the form in order to "live-update" a preview of the user's blog by listening for changes to the form.
+3. Have the event handler "live-update" a JSON snippet below the preview. 
+4. Have the event create a new article by using toHTML(), then add it to the articles.
+
+## Architecture
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Handlebars.js
+
+## Change Log
+
+## Credits and Collaborations
+Credit to John for doing the workshop on this project during lecture on 8/17/18.
---
+++ form-building/scripts/articleView.js
@@ -0,0 +1,133 @@
+'use strict';
+
+let articleView = {};
+
+articleView.populateFilters = () => {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      let val = $(this).find('address a').text();
+      let optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#author-filter option[value="${val}"]`).length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = () => {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-author="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = () => {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.handleMainNav = () => {
+  $('nav').on('click', '.tab', function(e) {
+    e.preventDefault();
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).fadeIn();
+  });
+
+  $('nav .tab:first').click();
+};
+
+articleView.setTeasers = () => {
+  $('.article-body *:nth-of-type(n+2)').hide();
+  $('article').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
+  });
+};
+
+// COMMENT: Where is this function called? Why?
+// This function is called in new.html because this js file is being used by multiple html files, therefore the code we want to run i new.html must be run as a function.
+articleView.initNewArticlePage = () => {
+  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show();
+
+  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+
+  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  $('form').on('change', 'input, textarea', articleView.create);
+};
+
+articleView.create = () => {
+  console.log('here2');
+  // TODO: Set up a variable to hold the new article we are creating.
+  // Clear out the #articles element, so we can put in the updated preview
+  let article = {};
+
+  // TODO: Instantiate an article based on what's in the form fields:
+  $('#articles').html('');
+
+
+  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  article.title = $('#title').val();
+  article.author = $('#author').val();
+  article.authorUrl = $('#authorUrl').val();
+  article.category = $('#category').val();
+  article.body = $('#body').val();
+  article.publishedOn = $('#publishedOn').val();
+
+  let post = new Article(article);
+
+  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('pre code').each();
+
+  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#articles').html(post.toHtml());
+  $('#JSON-output').show();
+  $('#JSON-output').val(JSON.stringify(article) + ',');
+
+};
+
+// COMMENT: Where is this function called? Why?
+// This function is called in index.html because this js file is being used by multiple html files, so this function runs the code we want for index.html.
+articleView.initIndexPage = () => {
+  articles.forEach(article => $('#articles').append(article.toHtml()));
+  articleView.populateFilters();
+  articleView.handleCategoryFilter();
+  articleView.handleAuthorFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+};
+
